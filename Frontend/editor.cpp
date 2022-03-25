388e0f226b75c9e1b0b59e9c30a8148024faf18d
LucAlexander <lucskyalex@gmail.com>
2022-03-25 16:54:06 -0400

    editor gui progress

--- Frontend/editor.cpp
+++ Frontend/editor.cpp
@@ -1,4 +1,6 @@
 #include "editor.h"
+#include "linkLabel.h"
+
 #include <QPalette>
 #include <QColor>
 #include <QLabel>
@@ -20,6 +22,7 @@ Editor::Editor(QWidget* parent):
 
 	auto* title = new QFrame(this);
 	title->setFrameStyle(QFrame::NoFrame);
+
 	title->setPalette(contentPalette);
 
 	auto* titleText = new QLabel(title);
@@ -34,10 +37,24 @@ Editor::Editor(QWidget* parent):
 	actions->setFrameStyle(QFrame::Panel | QFrame::Sunken);
 	actions->setPalette(contentPalette);
 
+
+	// test segment start
+	
+		auto* link = new LinkLabel(this);
+		link->setScaledContents(true);
+		link->setText("Link");
+		QObject::connect(
+			link, &LinkLabel::clicked,
+			link, &LinkLabel::printData
+		);
+
+	// test segment end
+
 	auto* grid = new QGridLayout(this);
 	grid->addWidget(title, 0, 0, 1, 2);
 	grid->addWidget(actions, 1, 0, 4, 1);
 	grid->addWidget(projects, 1, 1, 4, 1);
+	grid->addWidget(link, 2, 0, 1, 2);
 
 	setLayout(grid);
 
